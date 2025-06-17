fb0951a31149ab1905030b414637c4e056c1d2a5
andrei.halitchi <andrei.halitchi@zenithglobal.eu>
2025-06-17 16:43:03 +0200

    utilizzo full_backward (causa warning); specifica target size per la mapp; aggiunto metodo per la saliency loss

--- gradcam.py
+++ gradcam.py
@@ -9,7 +9,7 @@ class GradCAM:
         self.gradients = None
 
         self.target_layer.register_forward_hook(self.forward_hook)
-        self.target_layer.register_backward_hook(self.backward_hook)
+        self.target_layer.register_full_backward_hook(self.backward_hook)
 
     def forward_hook(self, module, input, output):
         self.activations = output
@@ -17,10 +17,16 @@ class GradCAM:
     def backward_hook(self, module, grad_input, grad_output):
         self.gradients = grad_output[0]
 
-    def generate(self):
+    def generate(self, target_size):
         weights = torch.mean(self.gradients, dim=(2, 3), keepdim=True)
         grad_cam = torch.sum(weights * self.activations, dim=1, keepdim=True)
         grad_cam = F.relu(grad_cam)
-        grad_cam = F.interpolate(grad_cam, size=(self.activations.shape[2], self.activations.shape[3]), mode='bilinear', align_corners=False)
+        grad_cam = F.interpolate(grad_cam, size=target_size, mode='bilinear', align_corners=False)
         grad_cam = (grad_cam - grad_cam.min()) / (grad_cam.max() - grad_cam.min() + 1e-8)
         return grad_cam.squeeze().detach().cpu().numpy()
+
+
+def compute_saliency_loss(gradcam, inputs, cam_size):
+    cam = gradcam.generate(target_size=cam_size)
+    cam_tensor = torch.from_numpy(cam).to(inputs.device)
+    return -torch.mean(cam_tensor)
